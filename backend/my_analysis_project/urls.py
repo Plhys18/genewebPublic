36d2a356b6524ed4b8d64e76227bf517cd1af964
AdrenalaNN <129157392+AdrenalaNN@users.noreply.github.com>
2025-02-27 07:34:01 +0100

    urls updtae

--- backend/my_analysis_project/urls.py
+++ backend/my_analysis_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from my_analysis_project.views import check_session
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/analysis/', include('analysis_app.urls')),
     path('api/auth/', include('auth_app.urls')),
+    path('api/analysis/', include('my_analysis_project.analysis.urls')),
+    path('api/check_session/', check_session, name="check_session"),
 ]
