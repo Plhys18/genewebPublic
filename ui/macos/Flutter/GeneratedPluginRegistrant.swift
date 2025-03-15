bc4c680103c4727e45f6b9154c418838e81c25ca
Lukas Nevosad <lukas@nevosad.cz>
2025-03-15 18:10:39 +0100

    Flůutter upgrade

--- ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import file_saver
 import path_provider_foundation
 import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
